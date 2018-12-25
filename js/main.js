3632f5f7c1f26d1b1c38948abfe40478e0d07662
wanggege919 <1914161290@qq.com>
2018-12-25 22:58:28 +0800

    适应手机

--- js/main.js
+++ js/main.js
@@ -26,35 +26,34 @@
     .santaclaus {
       display: block;
       margin: 0 auto;
-      max-width: 500px;
+      max-width: 334px;
   }
-  
   .santaclaus > div {
     margin: 0 auto;
-    max-width: 400px;
+    max-width: 267px;
   }
   /* 首先 是圣诞老人的帽子 */
   .santaclaus .chapeu {
       display: block;
       position: relative;
-      width: 310px;
+      width: 207px;
       z-index: 99;
   }
   .santaclaus .chapeu .cone-1 {
-      border-left: 50px solid transparent;
-      border-right: 20px solid transparent;
-      border-bottom: 70px solid #D32F2F;
+      border-left: 34px solid transparent;
+      border-right: 13px solid transparent;
+      border-bottom: 47px solid #D32F2F;
   }
   .santaclaus .chapeu .cone-2 {
       background-color:  #D32F2F;
       border-top: 0px solid transparent;
-      border-right: 340px solid #D32F2F;
-      border-bottom: 100px solid transparent;
+      border-right: 227px solid #D32F2F;
+      border-bottom: 67px solid transparent;
       border-color: transparent;
-      box-shadow: -25px 15px 30px -5px rgba(1,1,1,0.3);
+      box-shadow: -17px 10px 20px -3px rgba(1,1,1,0.3);
       position: relative;
-      left: -56px;
-      top: 49px;
+      left: -37px;
+      top: 33px;
       transform: rotate(-15deg);
       border-radius: 30% 100% 0 100%;
   }
@@ -65,53 +64,53 @@
     display: block;
   }
   .santaclaus .chapeu::after {
-    box-shadow: 0px -15px 40px -5px rgba(1,1,1,0.4);
-      height: 80px;
-      margin-left: -15px;
+    box-shadow: 0px -10px 27px -3px rgba(1,1,1,0.4);
+      height: 53px;
+      margin-left: -10px;
       position: relative;
-      width: 340px;
-      border-radius: 0.8em;
+      width: 227px;
+      border-radius: 0.5em;
   }
   .santaclaus .chapeu::before {
-    box-shadow: 20px 20px 40px -5px rgba(1,1,1,0.3);
-      height: 80px;
-      left: -120px;
+    box-shadow: 13px 13px 27px -3px rgba(1,1,1,0.3);
+      height: 53px;
+      left: -80px;
       position: absolute;
-      top: 80px;
-      width: 80px;
+      top: 53px;
+      width: 53px;
       z-index: 111;
       border-radius: 50%;
   }
   /* 这是脸 */
   .santaclaus .face {
       background-color: #ffccbc;
-      box-shadow: 0px 0px 30px 5px rgba(1,1,1,0.3);
+      box-shadow: 0px 0px 20px 3px rgba(1,1,1,0.3);
       content: " ";
       display: block;
-      height: 130px;
+      height: 87px;
       position: relative;
-      top: -10px;
-      width: 300px;
+      top: -7px;
+      width: 200px;
   }
   /* 这是耳朵 */
   .santaclaus > .orelhas {
       background-color: #eea2ad;
-      box-shadow: 0px 0px 30px 5px rgba(1,1,1,0.3);
+      box-shadow: 0px 0px 20px 3px rgba(1,1,1,0.3);
       display: block;
-      height: 60px;
+      height: 40px;
       margin: 0 auto;
       position: relative;
-      top: -125px;
-      width: 330px;
+      top: -83px;
+      width: 220px;
       z-index: -1;
-      border-radius: 1em 1em 2em 2em;
+      border-radius: 1em 1em 2em 2em;//
   }
   /* 这是眼睛 */
   .santaclaus .face > .eyes {
     display: flex;
     margin: 0 auto;
-    padding-top: 20px;
-    max-width: 160px;
+    padding-top: 14px;
+    max-width: 106px;
     width: 100%;
     justify-content: space-between;
   }
@@ -121,17 +120,17 @@
     background-color: #111;
     content: " ";
     display: inline-block;
-    height: 20px;
-    width: 20px;
+    height: 13px;
+    width: 13px;
     border-radius: 50%;
   }
   /* 这是鼻子 */
   .santaclaus .face > .nariz {
     background-color: #EEA2AD;
-    height: 50px;
+    height: 33px;
     margin: 0 auto;
     position: relative;
-    width: 70px;
+    width: 47px;
     z-index: 111; 
     border-radius: 50%;
   }
@@ -140,66 +139,32 @@
   /* 首先是圣诞老人摇摆的大胡子 */
   .santaclaus .face > .barba {
       background-color: #ecf0f1;
-      box-shadow: -10px 10px 10px -5px rgba(1,1,1,0.3);
-      height: 240px;
+      box-shadow: -7px 7px 7px -3px rgba(1,1,1,0.3);
+      height: 160px;
       margin: 0 auto;
       position: relative;
-      top: -30px;
-      width: 340px;
-      left: -20px;
+      top: -20px;
+      width: 227px;
+      left: -13px;
       transform: rotate(-20deg);
       border-radius: 0% 100% 70% 100%;
       animation: animaBarba 1.6s ease-in-out infinite alternate;
   }
   /* 圣诞老人的嘴巴 */
   .santaclaus .face > .barba > .boca {
-      height: 50px;
-      left: 30px;
+      height: 33px;
+      left: 20px;
       margin: 0 auto;
       position: relative;
-      top: 20px;
-      width: 60px;
+      top: 13px;
+      width: 40px;
       border-radius: 50%;
       animation: hoho 1.6s ease-in-out infinite alternate;
   }
-  /* 动态的 HO HO HO*/
-  .hohoho {
-      height: 0;
-      padding-top: 200px;
-      width: 150px;
-      animation: txtHohoho 1.6s ease-in-out infinite alternate;
-  }
-  .hohoho h2::before {
-    text-shadow: 0px 10px 20px rgba(1,1,1,0.4);
-    color: #fff;
-    content: "HO";
-      font-size: 62px;
-      line-height: 1.4em;
-  }
   
-  .hohoho:last-of-type h2 {
-      margin: 0 10px 30px;
-      text-align: right;
-  }
-  .hohoho:last-of-type h2:nth-child(2) {margin-right: -50px;}
-  .hohoho:last-of-type h2:first-child,
-  .hohoho:last-of-type h2:last-child {
-    animation: txtHohohoRight 1.6s ease-in-out infinite alternate;
-  }
-  .hohoho:first-of-type h2 {
-      margin: 0 50px 30px 0;
-      text-align: left;
-  }
-  .hohoho:first-of-type h2:nth-child(2) {margin-left: -60px;}
-  .hohoho:first-of-type h2:first-child,
-  .hohoho:first-of-type h2:last-child {
-    animation: txtHohohoLeft 1.6s ease-in-out infinite alternate;
-  }
-  .hohoho h2:nth-child(2) {
-    animation: txtHohohoMiddle 1.6s ease-in-out infinite alternate;
-  }
   
-  /* 好啦,这个圣诞老人就送给你们啦! 
+  /* 好啦,这个圣诞老人就送给你们啦! 圣诞快乐哈!
+   *一个小玩意做了几个小时啊
    *快谢谢旺哥
    *哈哈哈~~~~
    */
